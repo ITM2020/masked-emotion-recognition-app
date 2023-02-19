5a8a9203104ec783c85794a2507c158cac445e42
Nathan <nathanhoang123@gmail.com>
2023-02-19 10:45:59 -0500

    Update callback function to perform detection and prediction on video stream

--- api/app.py
+++ api/app.py
@@ -42,6 +42,38 @@ st.write("To begin detecting the whether a person is wearing a mask or not and t
 
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.resize(img, (1280, 720))
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    num_faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+
+    for (x, y, w, h) in num_faces:
+        roi_frame = img[y:y + h, x:x + w]
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_frame, (128, 128)), -1), 0)
+        cropped_img_gray = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+
+        # predict whether wearing a mask
+        maskPredict = maskModel.predict(cropped_img)
+        maxindex = int(np.argmax(maskPredict))
+
+        # use mask or unmasked set to recognize emotion depending on masked/unmasked
+        if mask_dict[maxindex]=='Unmasked':
+            cv2.rectangle(img, (x, y - 50), (x + w, y + h + 10), (0, 0, 255), 4)
+            UnmaskedEmotionPredict = UnmaskedEmotion.predict(cropped_img_gray)
+            maxindex2 = int(np.argmax(UnmaskedEmotionPredict))
+            cv2.putText(img, emotion_dict[maxindex2], (x + 200, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(img, mask_dict[maxindex], (x + 5, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            continue
+
+        elif mask_dict[maxindex]=='Masked':
+            cv2.rectangle(img, (x, y - 50), (x + w, y + h + 10), (0, 255, 0), 4)
+            MaskedEmotionPredict = MaskedEmotion.predict(cropped_img_gray)
+            maxindex3 = int(np.argmax(MaskedEmotionPredict))
+            cv2.putText(img, emotion_dict[maxindex3], (x + 200, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(img, mask_dict[maxindex], (x + 5, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            continue
+        
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 webrtc_streamer(key="example", video_frame_callback=callback)
