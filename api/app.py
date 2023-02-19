d8d1c3b9ae11d6aeaac1ca4e98dcde80659c5558
Nathan <nathanhoang123@gmail.com>
2023-02-19 09:44:53 -0500

    Import libraries and Keras models

--- api/app.py
+++ api/app.py
@@ -1,97 +1,38 @@
-import os
-import sys
-
-# Flask
-from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-
-# Some utilites
+import cv2
 import numpy as np
-from util import base64_to_pil
+from keras.models import model_from_json
 
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
-# Declare a flask app
-app = Flask(__name__)
+import av
 
+mask_dict = {0: "Masked", 1: "Unmasked"}
+emotion_dict = {0: "Angry", 1: "Happy", 2: "Neutral", 3: "Surprised"}
 
-# You can use pretrained model from Keras
-# Check https://keras.io/applications/
-# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
+# Load masked/unmasked model
+json_file = open('models/MaskModel.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+maskModel = model_from_json(loaded_model_json)
+maskModel.load_weights("models/MaskModel.h5")
 
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
+# Load unmasked emotion recognition model
+json_file = open('models/EmotionUnmaskedModel.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+UnmaskedEmotion = model_from_json(loaded_model_json)
+UnmaskedEmotion.load_weights("models/EmotionUnmaskedModel.h5")
 
-print('Model loaded. Check http://127.0.0.1:5000/')
+# Load masked emotion recognition model
+json_file = open('models/EmotionMaskedModel.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+MaskedEmotion = model_from_json(loaded_model_json)
+MaskedEmotion.load_weights("models/EmotionMaskedModel.h5")
 
+# Load HaarCascade face classifier
+face_cascade = cv2.CascadeClassifier("models/HaarCascadeFiles/haarcascade_frontalface_default.xml")
+ds_factor = 0.6
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
-
-# Load your own trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-
-def model_predict(img, model):
-    img = img.resize((224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        # Get the image from post request
-        img = base64_to_pil(request.json)
-
-        # Save the image to ./uploads
-        # img.save("./uploads/image.png")
-
-        # Make prediction
-        preds = model_predict(img, model)
-
-        # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
-        # Serialize the result, you can add additional fields
-        return jsonify(result=result, probability=pred_proba)
-
-    return None
-
-
-if __name__ == '__main__':
-    # app.run(port=5002, threaded=False)
-
-    # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
+print("Models have been loaded.")
